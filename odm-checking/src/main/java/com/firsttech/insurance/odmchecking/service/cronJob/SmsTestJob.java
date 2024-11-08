410273b4d6418ff9509af794a897c9da1fdce5c9
PeterLiao <peter.liao@qianhe.com.tw>
2024-11-08 18:15:38 +0800

    add SMS job

---
+++ odm-checking/src/main/java/com/firsttech/insurance/odmchecking/service/cronJob/SmsTestJob.java
@@ -0,0 +1,22 @@
+package com.firsttech.insurance.odmchecking.service.cronJob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+import com.firsttech.insurance.odmchecking.service.SmsService;
+
+@Service
+public class SmsTestJob {
+	private final static Logger logger = LoggerFactory.getLogger(SmsTestJob.class);
+
+	@Autowired
+	private SmsService smsService;
+	
+	@Scheduled(cron = "0 0/10 20-21 * * ?")
+	public void sendSMSTesting() {
+		boolean isSuccess = smsService.sendSMS();
+		logger.info("[Test Job] SMS sending result: " + (isSuccess ? "SUCCESSFUL" : "FAIL"));
+	}
+}
